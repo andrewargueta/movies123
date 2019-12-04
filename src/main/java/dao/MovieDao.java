99d300fc1ac196cda05b884082268aee5cd2275f
liangandrew <andrew.liang@stonybrook.edu>
2019-12-04 00:02:57 -0500

    Update MovieDao.java

--- src/main/java/dao/MovieDao.java
+++ src/main/java/dao/MovieDao.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.*;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
