ed1a1b92cc651f51f2f9f665961f5062df982214
liangandrew <andrew.liang@stonybrook.edu>
2019-12-04 01:36:58 -0500

    Update RentalDao.java

--- src/main/java/dao/RentalDao.java
+++ src/main/java/dao/RentalDao.java
@@ -12,17 +12,32 @@ public class RentalDao {
 		List<Rental> rentals = new ArrayList<Rental>();
 			
 		/*Sample data begins*/
-		for (int i = 0; i < 4; i++) {
-			Rental rental = new Rental();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://mysql3.cs.stonybrook.edu:3306/agargueta?user=agargueta", "agargueta", "111456257");			
+			Statement st = con.createStatement();
+			customerID = customerID.replaceAll("-","");
+			String query = "SELECT AccountId, OrderId, MovieId, CustRepId "
+					+ "FROM (Customer INNER JOIN Account ON (Customer.Id=Account.CustomerId)) "
+					+ "INNER JOIN Rental ON (Account.Id=Rental.AccountId) "
+					+ "WHERE Customer.Id= ? ";	
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setString(1, customerID);
+			ResultSet rs = ps.executeQuery();
 			
-			rental.setOrderID(1);
-			rental.setMovieID(1);
-			rental.setCustomerRepID(1);
-		
-			rentals.add(rental);
-			
-			
+			while(rs.next()) {
+				Rental rental = new Rental();
+				rental.setOrderID(rs.getInt("OrderId"));
+				int r = rental.getOrderID();
+				rental.setMovieID(rs.getInt("MovieId"));
+				rental.setCustomerRepID(rs.getInt("CustRepId"));
+				
+				rentals.add(rental);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
 		}
+		
 		/*Sample data ends*/
 						
 		return rentals;
